1d7ae45e05e2445c0cd7cc82c0014ae70054fd2f
maning <maning@yqjr.com.cn>
2018-08-31 15:51:30 +0800

    删除无用代码

--- app/src/main/java/com/maning/mnupdateapk/Constants.java
+++ app/src/main/java/com/maning/mnupdateapk/Constants.java
@@ -15,8 +15,7 @@ public class Constants {
 
     public static final String APK_URL_01 = "http://download.fir.im/v2/app/install/56dd4bb7e75e2d27f2000046?download_token=e415c0fd1ac3b7abcb65ebc6603c59d9&source=update";
     public static final String APK_URL_02 = "http://download.fir.im/v2/app/install/5a52e936ca87a8600e0002f9?download_token=cd8662357947f151de92975b46082ba6&source=update";
-    public static final String APK_URL_04 = "http://cdn.hfenq.cn/app/android/shenguixy.apk";
-    public static final String APK_URL_05 = "https://www.zhuangzhujin.com/download/jinzhuangzhu.apk";
+
 
     public static final String APK_SAVE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MNUpdateAPK/update.apk";
 
